31e758a8022f5a932fd045c81dc068fb963dd308
kuranton <anton26kur@gmail.com>
2021-03-29 22:36:53 +0300

    fix unset initial rate

--- src/store/profileSlice.js
+++ src/store/profileSlice.js
@@ -6,23 +6,27 @@ export const profileSlice = createSlice({
     name: '',
     job: '',
     email: '',
+    rate: 0,
     certifications: [],
     about: '',
     country: '',
-    photoUrl: ''
+    photoUrl: '',
+    editing: false
   },
   reducers: {
-    setName: (state, action) => {state.name = action.payload},
-    setJob: (state, action) => {state.job = action.payload},
-    setEmail: (state, action) => {state.email = action.payload},
-    setAbout: (state, action) => {state.about = action.payload},
+    setName: (state, action) => {state.name = action.payload.trim()},
+    setJob: (state, action) => {state.job = action.payload.trim()},
+    setEmail: (state, action) => {state.email = action.payload.trim()},
+    setAbout: (state, action) => {state.about = action.payload.trim()},
     setRate: (state, action) => {state.rate = action.payload},
     setCountry: (state, action) => {state.country = action.payload},
     setPhotoUrl: (state, action) => {state.photoUrl = action.payload},
-    addCertification: (state) => {state.certifications.push(0)}
+    addCertification: (state) => {state.certifications.push(0)},
+    edit: (state) => {state.editing = true},
+    stopEditing: (state) => {state.editing = false}
   },
 })
 
-export const {setName, setJob, setEmail, setAbout, setRate, setCountry, setPhotoUrl, addCertification} = profileSlice.actions
+export const {setName, setJob, setEmail, setAbout, setRate, setCountry, setPhotoUrl, addCertification, edit, stopEditing} = profileSlice.actions
 
 export default profileSlice.reducer
