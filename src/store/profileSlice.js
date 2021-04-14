c71200ffa277699fab263e08bb2c42a167c839f2
kuranton <anton26kur@gmail.com>
2021-04-14 18:22:21 +0300

    unset default rate

--- src/store/profileSlice.js
+++ src/store/profileSlice.js
@@ -6,7 +6,7 @@ export const profileSlice = createSlice({
     name: '',
     job: '',
     email: '',
-    rate: 0,
+    rate: null,
     certifications: [],
     about: '',
     country: '',
@@ -14,10 +14,10 @@ export const profileSlice = createSlice({
     editing: false
   },
   reducers: {
-    setName: (state, action) => {state.name = action.payload.trim()},
-    setJob: (state, action) => {state.job = action.payload.trim()},
-    setEmail: (state, action) => {state.email = action.payload.trim()},
-    setAbout: (state, action) => {state.about = action.payload.trim()},
+    setName: (state, action) => {state.name = action.payload},
+    setJob: (state, action) => {state.job = action.payload},
+    setEmail: (state, action) => {state.email = action.payload},
+    setAbout: (state, action) => {state.about = action.payload},
     setRate: (state, action) => {state.rate = action.payload},
     setCountry: (state, action) => {state.country = action.payload},
     setPhotoUrl: (state, action) => {state.photoUrl = action.payload},
