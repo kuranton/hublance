3a7bed9944a753a2a0e6eae9aec5cf1c82300b56
kuranton <anton26kur@gmail.com>
2021-08-10 05:19:54 +0300

    server url from env

--- src/components/UserPic/UserPic.js
+++ src/components/UserPic/UserPic.js
@@ -3,7 +3,7 @@ import style from './UserPic.module.css'
 const UserPic = ({url, letter, onLoad}) => (
   <div className={style.wrap}>
     {url
-      ? <img className={style.photo} src={url} alt='' onLoad={onLoad}/>
+      ? <img className={style.photo} src={`${process.env.SERVER_URL}${url}`} alt='' onLoad={onLoad}/>
       : letter ?
         <div className={style.photoPlaceholder}>{letter}</div>
       : <div className={style.photoIcon}/>
