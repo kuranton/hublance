6abd91347e39d34a21d9978ea7ac53e94d584e11
kuranton <anton26kur@gmail.com>
2021-08-26 00:11:13 +0300

    measure text debug

--- src/util/getTextWidth.js
+++ src/util/getTextWidth.js
@@ -1,8 +1,10 @@
 export function getTextWidth (text, font) {
   // re-use canvas object for better performance
-  var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement('canvas'))
+  console.log(window.measureTextCanvas)
+  var canvas = window.measureTextCanvas || (window.measureTextCanvas = document.createElement('canvas'))
   var context = canvas.getContext('2d')
   context.font = font
   var metrics = context.measureText(text)
+  console.log(metrics)
   return metrics.width
 }
